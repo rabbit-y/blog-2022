48fb9deffb626335fe5972e471f3677bf43bc4a0
Yan <869043928@qq.com>
2022-07-13 16:16:20 +0800

    feat：名人名言

--- src/components/pages/nav/index.jsx
+++ src/components/pages/nav/index.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import { Menu } from "antd";
+import axios from "axios";
 import { useSelector, useDispatch } from "react-redux";
 import { setRouterKey } from "@/store";
 import { ROTER } from "@utils/variable";
@@ -15,9 +16,11 @@ const Nav = () => {
   const routerKey = useSelector((state) => state.routerKey);
   const info = useSelector((state) => state.info);
   const dispatch = useDispatch();
+  const [say, setSay] = useState("");
   useEffect(() => {
     const key = "/" + location.pathname.split("/")[1];
     dispatch(setRouterKey(key));
+    getSay();
   }, []);
   // 导航跳转
   const menuClick = ({ key }) => {
@@ -26,16 +29,24 @@ const Nav = () => {
     // 滚动处理
     scroll(0, 0);
   };
-  // 退出登录
-  const logout = () => {
-    navigate("/index");
+  const getSay = async () => {
+    const {
+      data: { code, data },
+    } = await axios.get(
+      `https://v2.alapi.cn/api/mingyan?token=LwExDtUWhF3rH5ib`
+    );
+    if (code === 200) {
+      setSay(data);
+    }
   };
   return (
     <div className="banner">
       <div className="title">
         <div className="title-left">
           <div className="title-name">{info.stationName}</div>
-          <div className="title-dec">{info.stationDec}</div>
+          <div className="title-dec">
+            {say.content ? say.content + "  ——" + say.author : info.stationDec}
+          </div>
         </div>
       </div>
       <img className="nav-img" src={cat2} />
